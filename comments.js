a5400896912ad974ea21667e8198de9b15bbb526
Tore M. Hirth <116200852+Torehirth@users.noreply.github.com>
2024-05-20 09:24:47 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,72 +1,44 @@
 // create web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-const querystring = require('querystring');
-
-const comments = [
-    {
-        name: '张三',
-        message: '今天天气不错！',
-        dateTime: '2019-5-17'
-    },
-    {
-        name: '李四',
-        message: '今天天气不错！',
-        dateTime: '2019-5-17'
-    },
-    {
-        name: '王五',
-        message: '今天天气不错！',
-        dateTime: '2019-5-17'
-    },
-    {
-        name: '赵六',
-        message: '今天天气不错！',
-        dateTime: '2019-5-17'
-    },
-    {
-        name: '田七',
-        message: '今天天气不错！',
-        dateTime: '2019-5-17'
+const http = require('http')
+const express = require('express')
+const app = express()
+const server = http.createServer(app)
+const PORT = 3000
+// connect to the database
+const mongoose = require('mongoose')
+const Comment = require('./models/comment')
+const DB_URL = 'mongodb://localhost:27017/comment'
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('Connected to MongoDB')
+})
+// configure the template engine
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.urlencoded({ extended: true }))
+// set up the routes
+app.get('/', (req, res) => {
+  Comment.find({}, (err, comments) => {
+    if (err) {
+      console.error(err)
+      return
     }
-];
-
-http.createServer((req, res) => {
-    const parseObj = url.parse(req.url, true);
-    const pathname = parseObj.pathname;
-    if (pathname === '/') {
-        fs.readFile(path.join(__dirname, 'views', 'index.html'), (err, data) => {
-            if (err) {
-                return res.end('404 Not Found.');
-            }
-            let htmlStr = data.toString();
-            let html = '';
-            comments.forEach(item => {
-                html += `
-                    <li>
-                        <p>${item.name}</p>
-                        <p>${item.message}</p>
-                        <p>${item.dateTime}</p>
-                    </li>
-                `;
-            });
-            htmlStr = htmlStr.replace('<!-- 评论列表 -->', html);
-            res.end(htmlStr);
-        });
-    } else if (pathname === '/post') {
-        fs.readFile(path.join(__dirname, 'views', 'post.html'), (err, data) => {
-            if (err) {
-                return res.end('404 Not Found.');
-            }
-            res.end(data);
-        });
-    } else if (pathname.indexOf('/public/') === 0) {
-        fs.readFile(path.join(__dirname, pathname), (err, data) => {
-            if (err) {
-                return res.end('404 Not Found.');
-            }
-            res.end(data);
-        });
+    res.render('index', { comments })
+  })
+})
+app.post('/', (req, res) => {
+  const comment = new Comment(req.body)
+  comment.save((err, comment) => {
+    if (err) {
+      console.error(err)
+      return
     }
+    res.redirect('/')
+  })
+})
+// start the server
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
